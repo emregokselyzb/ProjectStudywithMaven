f5f4ff01958b657d0dd9cea088f69ca6fe7d25c1
emregokselyzb <emre.g.yzb@gmail.com>
2023-08-07 23:34:33 +0300

    Initial commit

---
+++ src/test/java/Student.java
@@ -0,0 +1,19 @@
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Student {
+
+
+
+    private String name;
+    private int number;
+    private String mail;
+    private String password;
+
+}
---
+++ src/test/java/TaskOzzy/Color.java
@@ -0,0 +1,9 @@
+package TaskOzzy;
+
+public enum Color {
+
+    RED,GREEN,WHITE;
+
+
+
+}
